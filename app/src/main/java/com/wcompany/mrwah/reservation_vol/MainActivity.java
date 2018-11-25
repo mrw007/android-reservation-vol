5299ecbec08b95e975f3e5f707e711cb73e7690a
mrw007 <mr.wahib@gmail.com>
2018-11-25 22:16:42 +0100

    home page

--- app/src/main/java/com/wcompany/mrwah/reservation_vol/MainActivity.java
+++ app/src/main/java/com/wcompany/mrwah/reservation_vol/MainActivity.java
@@ -1,7 +1,10 @@
 package com.wcompany.mrwah.reservation_vol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final CardView liste_vols = findViewById(R.id.btn_liste_vols);
+        liste_vols.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListeVols.class);
+                startActivity(intent);
+            }
+        });
     }
 }
